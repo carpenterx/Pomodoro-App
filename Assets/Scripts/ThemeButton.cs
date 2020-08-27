f9c0ce1a0aad6f3b268871eb3e87b3ffe023b49e
Sorin <jordache_sor@yahoo.com>
2020-08-27 18:51:35 +0300

    added a new type of theme selection button

---
+++ Assets/Scripts/ThemeButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ThemeButton : MonoBehaviour
+{
+    public Image normalColorImage;
+    public Image highlightColorImage;
+
+    public ThemeColors themeColors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        normalColorImage.color = themeColors.normalColor;
+        highlightColorImage.color = themeColors.highlightedColor;
+    }
+}
