ce1f1aa8a29bb2ac896e3a8196c57c122ff24843
Sorin <jordache_sor@yahoo.com>
2021-02-05 17:46:50 +0200

    added background image loading

---
+++ Assets/Scripts/BackgroundImage.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using SFB;
+
+public class BackgroundImage : MonoBehaviour
+{
+    public Image image;
+
+    public void BrowseToBackGround()
+    {
+        var extenstions = new[] { new ExtensionFilter("Image Files", "jpg", "png") };
+        string[] selectedFiles = StandaloneFileBrowser.OpenFilePanel("Open Sound File", "", extenstions, false);
+        if(selectedFiles.Length > 0)
+        {
+            ChangeBackgroundImage(selectedFiles[0]);
+        }
+    }
+
+    private void ChangeBackgroundImage(string imagePath)
+    {
+        byte[] imageData = File.ReadAllBytes(imagePath);
+        Texture2D texture = new Texture2D(64, 64, TextureFormat.ARGB32, false);
+        texture.LoadImage(imageData);
+        texture.name = Path.GetFileNameWithoutExtension(imagePath);
+        Sprite mySprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        image.sprite = mySprite;
+    }
+}
