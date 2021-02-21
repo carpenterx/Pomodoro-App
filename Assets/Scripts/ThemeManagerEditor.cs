568cc9ff0291c218ddad87f4ada689025d936a1e
Sorin <jordache_sor@yahoo.com>
2021-02-21 19:03:10 +0200

    added  code to connect themables to the theme manager

---
+++ Assets/Scripts/ThemeManagerEditor.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ThemeManager))]
+public class ThemeManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //ThemeManager themeManager = (ThemeManager)target;
+
+        DrawDefaultInspector();
+
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Add Themables"))
+        {
+            AddThemables();
+        }
+    }
+
+    private void AddThemables()
+    {
+        ThemeManager themeManager = (ThemeManager)target;
+
+        themeManager.themablesList.Clear();
+        Object[] objects = Resources.FindObjectsOfTypeAll(typeof(Themable));
+        foreach (Themable themable in objects)
+        {
+            themeManager.themablesList.Add(themable);
+        }
+    }
+}
+#endif
